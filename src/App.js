0edad033fea4b013dca6b67b791f34ab7a4f4458
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-09-15 17:53:09 +0530

    routes updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Home from "./pages/Home";
 import './App.css'
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Signin from "./pages/Signin";
 import Register from "./pages/Register";
 
@@ -12,11 +12,10 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/">
-            <Route index element={<Home />} />
-            <Route path="signin" element={<Signin />} />
-            <Route path="register" element={<Register />} />
-          </Route>
+          <Route index path="/home" element={<Home />} />
+          <Route exact path="/" element={<Navigate to="/home" />} />
+          <Route path="signin" element={<Signin />} />
+          <Route path="register" element={<Register />} />
         </Routes>
         <Footer />
       </BrowserRouter>
